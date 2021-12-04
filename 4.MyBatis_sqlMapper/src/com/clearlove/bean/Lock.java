7b1d7811c6bf4b3ca2bff9b9229d63dc6a9f9db6
Promise <976949689@qq.com>
2021-12-04 17:28:33 +0800

    2021年12月4日17:28:28

--- 4.MyBatis_sqlMapper/src/com/clearlove/bean/Lock.java
+++ 4.MyBatis_sqlMapper/src/com/clearlove/bean/Lock.java
@@ -1,5 +1,7 @@
 package com.clearlove.bean;
 
+import java.util.List;
+
 /**
  * 锁表
  * @author promise
@@ -10,6 +12,26 @@ public class Lock {
   private Integer id;
 
   private String lockName;
+
+  // 查询锁的时候把所有的钥匙也查出来
+  private List<Key> keys;
+  // 1-1关联  1-n关联  n-n关联
+  // 一个钥匙开一把锁  1-1
+  // 从lock看钥匙  1-n
+  // 从key来看lock n-1
+  // n-n:
+  // student表 teacher表
+  // 1-n: n-1; n-n 外键应该放在哪个表？
+  // 一对多：外键一定放在多的一端
+  // n-n: 中间表存储对应关系
+
+  public List<Key> getKeys() {
+    return keys;
+  }
+
+  public void setKeys(List<Key> keys) {
+    this.keys = keys;
+  }
 
   public Integer getId() {
     return id;
@@ -32,6 +54,9 @@ public class Lock {
     return "Lock{" +
         "id=" + id +
         ", lockName='" + lockName + '\'' +
+        ", keys=" + keys +
         '}';
   }
+
+
 }
