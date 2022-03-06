2cdfca4b08d68564176d1345634c9116b7189a93
Promise <976949689@qq.com>
2022-03-06 23:19:49 +0800

    2022年3月6日23:19:35

---
+++ 8.MyBatis_mbg/src/com/clearlove/test/MyBatisCRUDTest.java
@@ -0,0 +1,29 @@
+package com.clearlove.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author promise
+ * @date 2021/10/31 - 17:40
+ */
+public class MyBatisCRUDTest {
+
+  // 工厂一个
+  SqlSessionFactory sqlSessionFactory;
+
+  @Before
+  public void initSqlSessionFactory() throws IOException {
+    String resource = "mybatis-config.xml";
+    InputStream inputStream = Resources.getResourceAsStream(resource);
+    sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+  }
+
+
+}
